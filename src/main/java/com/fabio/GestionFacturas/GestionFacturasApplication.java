d0ab810679d7c70605b0b28ef4fbd6958d2cc51f
hagi33 <fabiorook9000@gmail.com>
2026-08-26 16:56:40 +0200

    feat: primer commit del proyecto

---
+++ src/main/java/com/fabio/GestionFacturas/GestionFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.fabio.GestionFacturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio/GestionFacturas/GestionFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio.GestionFacturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
